d30f57053f8125a322cfbf4219206e7f265ee79e
Shu1237 <ttei8191@gmail.com>
2025-09-24 07:11:23 +0700

    fix: remove leading slashes from AUTH_ENDPOINT paths

--- FrontEnd/src/lib/api/endpoint.ts
+++ FrontEnd/src/lib/api/endpoint.ts
@@ -1,11 +1,11 @@
 
 
 export const AUTH_ENDPOINT = {
-    REGISTER: "/Auth/register",
-    LOGIN: "/Auth/login",
-    VERIFY_CODE: "/Auth/verify-otp",
+    REGISTER: "Auth/register",
+    LOGIN: "Auth/login",
+    VERIFY_CODE: "Auth/verify-otp",
     RESET_PASSWORD: "Auth/forgot-password",
-    VERIFY_FORGOT_PASSWORD: "/Auth/verify-forgot-password",
-    REFRESH_TOKEN: "/Auth/refresh-token",
-    CHANGE_PASSWORD: "/Auth/change-password"
+    VERIFY_FORGOT_PASSWORD: "Auth/verify-forgot-password",
+    REFRESH_TOKEN: "Auth/refresh-token",
+    CHANGE_PASSWORD: "Auth/change-password"
 };
